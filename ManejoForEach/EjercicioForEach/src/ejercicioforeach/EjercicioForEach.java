3371c4c2e60a4bf7dd1e8ac7b06ccb726784ebad
fernandomurcia1 <fermupa1982@gmail.com>
2019-10-24 14:33:57 -0500

    ForEach
    En la clase Persona: Se generan atributos, constructor de la clase, metodos get and set.
    En la clase EjecicioForEach: Se crea arreglo de tipo personas, se implementa forEach donde accede a el metodo persona

--- ManejoForEach/EjercicioForEach/src/ejercicioforeach/EjercicioForEach.java
+++ ManejoForEach/EjercicioForEach/src/ejercicioforeach/EjercicioForEach.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicioforeach;
 
 /**
@@ -11,11 +6,24 @@ package ejercicioforeach;
  */
 public class EjercicioForEach {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // creamos un arreglo de personas
+        Persona[] personas = {new Persona("Luciana"), new Persona("Karen")};
+        
+        // Iteramos cada elemento del arreglo de personas 
+        for(Persona p : personas){
+            //Accedemos a las propiedades y/o metodos del objeto
+            String nombre = p.getNombre();
+            System.out.println("Nombre persona: " + nombre);
+        }
+        
+        System.out.println("");
+        //Creamos un arreglo de enteros
+        int[] edades = {2, 29, 37, 16};
+        //Iteramos el arreglo
+        for(int edad : edades){
+            System.out.println("Edad: " + edad);
+        }    
     }
-    
 }
--- ManejoForEach/EjercicioForEach/src/ejercicioforeach/Persona.java
+++ ManejoForEach/EjercicioForEach/src/ejercicioforeach/Persona.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicioforeach;
 
 /**
@@ -10,5 +5,31 @@ package ejercicioforeach;
  * @author fernando.murcia
  */
 public class Persona {
+    
+    private final int idPersona;
+    private String nombre;
+    
+    private static int contadorPersonas;
+    
+    Persona(String nombre){
+        idPersona = ++contadorPersonas;
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
     
 }
