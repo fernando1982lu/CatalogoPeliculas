1bf3bf2f19608b13dfb79fe98ec0c636aa024050
fernandomurcia1 <fermupa1982@gmail.com>
2019-10-24 14:39:27 -0500

    ForEach
    SSe realiza correccion en codigo errado

--- ManejoForEach/EjercicioForEach/src/ejercicioforeach/Persona.java
+++ ManejoForEach/EjercicioForEach/src/ejercicioforeach/Persona.java
@@ -16,16 +16,9 @@ public class Persona {
         this.nombre = nombre;
     }
 
-    /**
-     * @return the nombre
-     */
     public String getNombre() {
         return nombre;
     }
-
-    /**
-     * @param nombre the nombre to set
-     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
