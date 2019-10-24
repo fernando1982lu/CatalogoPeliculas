c23f80ae0151a5872f354a475106fc17bd89cb1c
fernandomurcia1 <fermupa1982@gmail.com>
2019-10-24 16:48:56 -0500

    Modificadores de Acceso
    Se definen atributos, constructores y metodos en la claseA

--- Modificadores de Acceso/EjercicioModificadoresAcceso/src/paqueteA/ClaseA.java
+++ Modificadores de Acceso/EjercicioModificadoresAcceso/src/paqueteA/ClaseA.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paqueteA;
 
 /**
@@ -11,4 +6,48 @@ package paqueteA;
  */
 public class ClaseA {
     
+    //Definicion de atributos
+    public int atrPublico = 5;
+    protected int atrProtegido = 6;
+    int atrPaquete = 7;
+    private int atrPrivado = 8;
+    
+    //Constructores
+    
+    public ClaseA(){
+        
+    }
+    
+    public ClaseA(int i){
+        System.out.println("Constructor publico 1");
+    }
+    
+    public ClaseA(int i, int j){
+        System.out.println("Constructor protected 2");
+    }
+    
+    public ClaseA(int i, int j, int k){
+        System.out.println("Constructor package 3");
+    }
+    
+    public ClaseA(int i, int j, int k, int l){
+        System.out.println("Constructor privado 4");
+    }
+    
+    //Definicion de métodos
+    public int metodoPublico(){
+        return 9;
+    }
+    
+    public int metodoProtegido(){
+        return 10;
+    }
+    
+    int metodoPaquete(){
+        return 11;
+    }
+    
+    private int metodoprivado(){
+        return 12;
+    }
 }
